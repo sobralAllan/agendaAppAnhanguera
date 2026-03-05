039206d242e6f40856839a967d0f7746fbf48a42
sobralAllan <allansobral2@hotmail.com>
2026-03-05 20:47:39 -0300

    Segundo commit

--- app/src/main/java/com/sobral/agendaapp/MainActivity.java
+++ app/src/main/java/com/sobral/agendaapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.sobral.agendaapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    public EditText codigo;
+    public EditText assunto;
+    public EditText date;
+    public EditText descricao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-}
+    }//fim do método
+
+    //Coletar os dados do formulário
+    public void coletarDadosFormulario(View view){
+        this.codigo    = findViewById(R.id.codigo);//Coletando o dado da tela e passando para a variável código
+        this.assunto   = findViewById(R.id.assunto);//Coletando o dado da tela e passando para a variável código
+        this.date      = findViewById(R.id.date);//Coletando o dado da tela e passando para a variável código
+        this.descricao = findViewById(R.id.descricao);//Coletando o dado da tela e passando para a variável código
+    }//fim do coletarDados
+
+
+
+}//fim da classe
